00e76148c8c4c6e3f06a2e03f74cc0a5fb06b164
bindhu <bindubaddam137@gmail.com>
2024-10-02 16:25:19 -0500

    created custom button component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import Button from "./Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 
 function App() {
+  const handleClick = () => {
+    console.log("Click!");
+  };
   return (
     <div>
       <div>
-        <Button>hi there</Button>
+        <Button
+          onClick={handleClick}
+          className="mb-5"
+          secondary
+          rounded
+          outline
+        >
+          <GoBell />
+          hi there
+        </Button>
       </div>
 
       <div>
-        <Button>hi there</Button>
+        <Button danger outline onMouseEnter={handleClick}>
+          <GoCloudDownload />
+          hi there
+        </Button>
       </div>
 
       <div>
-        <Button>bye there</Button>
+        <Button warning onMouseLeave={handleClick}>
+          <GoDatabase />
+          bye there
+        </Button>
       </div>
 
       <div>
-        <Button>doerg</Button>
+        <Button secondary>doerg</Button>
+      </div>
+
+      <div>
+        <Button primary rounded>
+          smtg
+        </Button>
       </div>
     </div>
   );
