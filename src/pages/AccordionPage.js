b6849f890f7722cbf751be238548a5079a2220f7
bindhu <bindubaddam137@gmail.com>
2024-10-09 10:27:34 -0500

    colors added

---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,26 @@
+import Accordion from "../components/Accordion";
+
+function AccordionPage() {
+  const items = [
+    {
+      id: "123",
+      label: "can i use React on a project?",
+      content:
+        "In the upper-right corner of any page, click , and then click Import repository. On the ",
+    },
+    {
+      id: "345",
+      label: "can i use React on a project?",
+      content:
+        "In the upper-right corner of any page, click , and then click Import repository. On the",
+    },
+    {
+      id: "567",
+      label: "can i use React on a project?",
+      content:
+        "In the upper-right corner of any page, click , and then click Import repository. On the",
+    },
+  ];
+  return <Accordion items={items} />;
+}
+export default AccordionPage;
